dbdc6021889fff6c063b880925ed2e5f86d8b9f0
1Huy2409 <nhathuy2409@gmail.com>
2025-01-06 00:47:50 +0700

    half project

--- js/script.js
+++ js/script.js
@@ -11,3 +11,66 @@ if (navbarButton)
         }   
     })
 }
+const workSection = document.querySelector(".work-section");
+const workPicture = workSection.querySelector(".work-picture");
+const imageWork = workPicture.querySelector("img");
+if (workSection)
+{
+    const workItems = workSection.querySelectorAll(".work-item");
+    if (workItems)
+    {
+        workItems.forEach(button =>
+            button.addEventListener("click", () =>
+            {
+                if (button.classList.contains("first-work"))
+                {
+                    if (imageWork)
+                    {
+                        imageWork.src = "./img/pic5.jpg";
+                    }
+                    workItems.forEach(item =>
+                        {
+                            if (item.classList.contains("active"))
+                            {
+                                item.classList.remove("active");
+                            } 
+                        }
+                        )
+                    button.classList.add("active");
+                }
+                if (button.classList.contains("second-work"))
+                {
+                    if (imageWork)
+                    {
+                        imageWork.src = "./img/pic6.jpg";
+                    }
+                    workItems.forEach(item =>
+                        {
+                            if (item.classList.contains("active"))
+                            {
+                                item.classList.remove("active");
+                            } 
+                        }
+                        )
+                    button.classList.add("active");
+                }
+                if (button.classList.contains("third-work"))
+                {
+                    if (imageWork)
+                    {
+                        imageWork.src = "./img/pic7.jpg";
+                    }
+                    workItems.forEach(item =>
+                        {
+                            if (item.classList.contains("active"))
+                            {
+                                item.classList.remove("active");
+                            } 
+                        }
+                        )
+                    button.classList.add("active");
+                }
+            })
+        )
+    }
+}
