eec1df64ed0b47f4f6984fc0856119a7f877bfb4
1Huy2409 <nhathuy2409@gmail.com>
2025-02-08 20:31:00 +0700

    fix feedback mentor

--- js/script.js
+++ js/script.js
@@ -86,6 +86,7 @@ if (commentItems)
             // console.log(item);
             item.addEventListener("click", ()=>
             {
+                item.classList.toggle('active');
                 const content = item.querySelector(".comment-content");
                 if (content)
                 {
@@ -96,3 +97,83 @@ if (commentItems)
     }
     )
 }
+// animation scroll and load
+// check element in viewport
+function isInViewport(element)
+{
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top + rect.height / 2 <= window.innerHeight &&
+        rect.bottom - rect.height / 2 >= 0
+    );
+}
+// end check element in vp
+const basicScrollLoad = (element, animation, state) =>
+{
+    if (isInViewport(element) && !state.hasAnimation)
+    {
+        element.classList.add(animation);
+        state.hasAnimation = true;
+    }
+}
+const contactScrollLoad = (parent, child, animation, state) =>
+{
+    if (isInViewport(parent) && !state.hasAnimation)
+    {
+        child.classList.add(animation);
+        state.hasAnimation = true;
+    }
+}
+const featureScrollLoad = (element, features, animation, state) =>
+{
+    if (isInViewport(element) && !state.hasAnimation)
+    {
+        features.forEach((item, index) =>
+        {
+            setTimeout(()=>
+            {
+                item.classList.add(animation);
+            }, index*400)
+        })
+        state.hasAnimation = true;
+    }
+}
+const pricingScrollLoad = (element, priceTypes, animation, state) =>
+{
+    if (isInViewport(element) && !state.hasAnimation)
+    {
+        priceTypes.forEach((index, item) =>
+        {
+            setTimeout(() =>
+            {
+                item.classList.add(animation);
+            }, index*400)
+        })
+        state.hasAnimation = true;
+    }
+}
+const discoverStatus = {hasAnimation: false};
+const portfolioStatus = {hasAnimation: false};
+const featuresStatus = {hasAnimation: false};
+const pricingStatus = {hasAnimation: false};
+const contactStatus = {hasAnimation: false};
+
+// querySelector
+const discoverImg = document.querySelector('.discover-img');
+const portfolioBox = document.querySelector('.con-2');
+const featureSection = document.querySelector('.feature-section');
+const featureItems = featureSection.querySelectorAll('.col-features');
+const features = [...featureItems];
+const priceSection = document.querySelector('.price-section');
+const priceBoxes = priceSection.querySelectorAll('.inner-box');
+const priceArray = [...priceBoxes];
+const contactSection = document.querySelector('.contact-section');
+const contactBox = contactSection.querySelector('.contact-box');
+window.addEventListener("scroll", () =>
+{
+    basicScrollLoad(discoverImg, "slideInLeft", discoverStatus);
+    basicScrollLoad(portfolioBox, "fadeIn", portfolioStatus);
+    featureScrollLoad(featureSection, features, "fadeIn", featuresStatus);
+    featureScrollLoad(priceSection, priceArray, "bounceIn", pricingStatus);
+    contactScrollLoad(contactSection, contactBox, "bounceIn", contactStatus);
+})
